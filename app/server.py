376783019048c03d1d2f177a3c73c757d22da370
acary <info@andycary.com>
2019-12-12 12:54:35 -0600

    added oos handling

--- app/server.py
+++ app/server.py
@@ -40,39 +40,70 @@ routes = [
     Mount('/static', StaticFiles(directory='app/static'), name='static')
 ]
 
-# ULMFiT
-model_file_name = 'ULMFiT_classifier_model_cpu'
+# Class vocabulary
 classes = ['account_blocked','application_status','apr','balance','bill_balance','bill_due','card_declined','credit_limit','credit_limit_change','credit_score','damaged_card','direct_deposit','exchange_rate','expiration_date','freeze_account','improve_credit_score','insurance','insurance_change','interest_rate','international_fees','min_payment','new_card','oos','order_checks','pay_bill','pin_change','redeem_rewards','replacement_card_duration','report_fraud','report_lost_card','rewards_balance','rollover_401k','taxes','transactions','transfer']
 
+# ULMFiT
+model_file_name = 'ULMFiT_classifier_model_cpu'
 async def setup_learner():
     ''' Load model '''
     learn = load_learner(path, 'models/ULMFiT_classifier_model_cpu.pkl')
-
     return learn
 
+
+# OOS
+model_file_name = 'ULMFiT_classifier_model_OOS_Plus_cpu'
+async def setup_learner_oos():
+    ''' Load OOS model '''
+    learn_oos = load_learner(path, 'models/ULMFiT_classifier_model_OOS_Plus_cpu.pkl')
+    return learn_oos
+
+
 loop = asyncio.get_event_loop()
+
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+
+tasks_oos = [asyncio.ensure_future(setup_learner_oos())]
+learn_oos = loop.run_until_complete(asyncio.gather(*tasks_oos))[0]
+
 loop.close()
 
 
+# Run app
 app = Starlette(debug=True, routes=routes)
 app.add_middleware(CORSMiddleware, allow_origins=[
                    '*'], allow_headers=['X-Requested-With', 'Content-Type'])
 
 
+# Handle message
 @app.route("/create-entry", methods=["POST"])
 async def create_entry(request):
     ''' Process and analyze new message entry '''
     print("Send button clicked!")
 
+    # Resolve out-of-scope queries
+    async def resolve_oos():
+        print("Resolving oos...")
+
+    # Retrieve message from request
     data = await request.json()
     message = data['message']
 
+    # Evaluate prediction
     x = learn.predict(message)
     y = str(x[0])
     print(y)
 
+    # Handle out-of-scope queries
+    if y == 'oos':
+        print("Identified as Out-of-Scope")
+        y = learn_oos.predict(y)
+        z = str(y[0])
+        print(z)
+        y = z
+
+    # Return final classification result
     return JSONResponse({'result': 'I have identified your question to be in the category: ' + y})
 
 if __name__ == '__main__':
